940d317a29956f52cc5c9fa00166e0c37afc56ac
Tim Burks <tim@neontology.com>
2007-11-08 19:48:44 -0800

    Removed empty files.

--- objc/Scriptability.m
+++ objc/Scriptability.m
@@ -30,11 +30,12 @@
                                                             NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
  */
+/*
 #import <Cocoa/Cocoa.h>
 #import "AppController.h"
 #import "MyDocument.h"
 #import "CatalogController.h"
-/*
+
 @class PackModel;
 
 @interface NSObject (MNscriptability)
